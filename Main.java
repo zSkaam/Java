c3b08421fe8f9e25f99267029620864c75c60fd0
Daniel Torres <144837614+zSkaam@users.noreply.github.com>
2026-08-11 09:32:51 -0300

    Add Main class with student presentation

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main { 
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "Daniel";
+        aluno1.idade = 22;
+        
+        aluno1.apresentar();
+    }
+}

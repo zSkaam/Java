f6baee0c4649d161886837cae760ee5961e7f59e
Daniel Torres <144837614+zSkaam@users.noreply.github.com>
2026-08-25 10:21:46 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main { 
-    public static void main(String[] args) {
-        Aluno aluno1 = new Aluno();
-        aluno1.nome = "Daniel";
-        aluno1.idade = 22;
-        
-        aluno1.apresentar();
-    }
-}

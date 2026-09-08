64d3230a391765dec7071e1350cc3bc625284ea0
DT <144837614+zSkaam@users.noreply.github.com>
2026-09-08 09:57:09 -0300

    Create SomadeNumeros.java

---
+++ SomadeNumeros.java
@@ -0,0 +1,9 @@
+public class SomadeNumeros {
+    public static void main(String[]args){
+        int soma = 0;
+        for (int i = 1;i<= 10; i++){
+            soma +=i;
+        }
+            System.out.println("A soma dos números é de 1 a 10 é:" + soma);
+    }
+}

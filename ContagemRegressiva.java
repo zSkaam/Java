2d43acf7b2f0f7ee70a16a315db4d8fdcd923288
DT <144837614+zSkaam@users.noreply.github.com>
2026-09-08 09:47:16 -0300

    Create ContagemRegressiva.java

---
+++ ContagemRegressiva.java
@@ -0,0 +1,8 @@
+public class ContagemRegressiva {
+    public static void main(String[]args){
+        for (int i = 10;i>= 1; i--){
+            System.out.println("Contagem regressiva:" + i);
+        }
+        System.out.println("Fim da contagem!");
+    }
+}
